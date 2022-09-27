4c48edc47ab4c2dbc08f74064f9edebd525286d6
Diana Olivas <dolivas644@gmail.com>
2022-09-27 14:25:34 -0700

    Displaying all 3 databases

--- client/src/components/dataList/IndividualsInfo.js
+++ client/src/components/dataList/IndividualsInfo.js
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react";
 import React from "react";
 
 const IndividualsInfo = () =>{
+    const [individuals, setIndividuals] = useState([]);
+    
+    //get species data table
+    const getIndividuals = async () =>{
+        const response = await fetch(
+            `http://localhost:4040/individuals`
+        );
+        const individual = await response.json();
+        console.log(individual );
+        setIndividuals(individual );
+    };
+
+    useEffect(() => {
+        getIndividuals();
+    }, []);
+
     return(
 <>
+<header> Individuals Data Table </header>
+<table>
+    <thead>
+        <th>ID: </th>
+        <th>Nickname:  </th>
+        <th>Seen On: </th>
+        <th>Species ID: </th>
+    </thead>
+    <tbody>
+    {individuals.map((individual, index) => {
+        return(
+        <tr key={index}>
+<td>{individual.id}</td>
+<td>{individual.nick_name}</td>
+<td>{individual.seen_on}</td>
+<td>{individual.species_id}</td>
+        </tr>
+   ) 
+   })}
+   </tbody>
+</table>
 </>
     );
 }
--- client/src/components/dataList/SightingsInfo.js
+++ client/src/components/dataList/SightingsInfo.js
@@ -1,8 +1,52 @@
+import { useEffect, useState } from "react";
 import React from "react";
 
 const SightingsInfo = () =>{
+    const [sightings, setSightings] = useState([]);
+    
+    //get species data table
+    const getSightings = async () =>{
+        const response = await fetch(
+            `http://localhost:4040/sightings`
+        );
+        const sighting = await response.json();
+        console.log(sighting );
+        setSightings(sighting );
+    };
+
+    useEffect(() => {
+        getSightings();
+    }, []);
+
     return(
 <>
+<header> Sightings Data Table </header>
+<table>
+    <thead>
+        <th>ID: </th>
+        <th>Date Time:  </th>
+        <th>Location: </th>
+        <th>Healthy: </th>
+        <th>Individual ID: </th>
+        <th>Created On: </th>
+        <th>Email: </th>
+    </thead>
+    <tbody>
+    {sightings.map((sighting, index) => {
+        return(
+        <tr key={index}>
+<td>{sighting.id}</td>
+<td>{sighting.date_time}</td>
+<td>{sighting.location}</td>
+<td>{sighting.healthy}</td>
+<td>{sighting.individual_id}</td>
+<td>{sighting.created_on}</td>
+<td>{sighting.email}</td>
+        </tr>
+   ) 
+   })}
+   </tbody>
+</table>
 </>
     );
 }
