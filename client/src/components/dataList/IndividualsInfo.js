b4b657719ec63bcf21b00c4be0df5e24048285b0
Diana Olivas <dolivas644@gmail.com>
2022-09-27 20:54:00 -0700

    Add Individuals to table & db Working

--- client/src/components/dataList/IndividualsInfo.js
+++ client/src/components/dataList/IndividualsInfo.js
@@ -1,47 +1,178 @@
 import { useEffect, useState } from "react";
 import React from "react";
+import { useReducer } from "react";
 
-const IndividualsInfo = () =>{
+// create useReducer for changing values
+const reducer = (state, action) => {
+    console.log(action, 'this is the action');
+    switch (action.type) {
+        case 'editID':
+            console.log('Logged if the editName action is being dispatched');
+            //it updates the name to the input value
+            return { ...state, id: action.payload };
+
+        case 'editNickName':
+            return { ...state, nick_name: action.payload };
+
+        case 'editSpeciesId':
+            return { ...state, species_id: action.payload };
+
+        case 'editSeenOn':
+            return { ...state, seen_on: action.payload };
+
+        case 'clearForm':
+            return { id: "", nick_name: '', species_id: '', seen_on: ''};
+        default:
+            return state;
+    }
+};
+
+const IndividualsInfo = () => {
+
     const [individuals, setIndividuals] = useState([]);
-    
+
     //get individuals data table
-    const getIndividuals = async () =>{
+    const getIndividuals = async () => {
         const response = await fetch(
             `http://localhost:4040/individuals`
         );
         const individual = await response.json();
-        console.log(individual );
-        setIndividuals(individual );
+        console.log(individual);
+        setIndividuals(individual);
     };
 
     useEffect(() => {
         getIndividuals();
     }, []);
 
-    return(
-<>
-<header> Individuals Data Table </header>
-<table>
-    <thead>
-        <th>ID: </th>
-        <th>Nickname:  </th>
-        <th>Seen On: </th>
-        <th>Species ID: </th>
-    </thead>
-    <tbody>
-    {individuals.map((individual, index) => {
-        return(
-        <tr key={index}>
-<td>{individual.id}</td>
-<td>{individual.nick_name}</td>
-<td>{individual.seen_on}</td>
-<td>{individual.species_id}</td>
-        </tr>
-   ) 
-   })}
-   </tbody>
-</table>
-</>
+
+       //initialistate of the form will be empty
+       const initialState = {
+        id: "",
+        nick_name: '',
+        species_id: '',
+        seen_on: '',
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+    console.log(state);
+
+    const handleAddIndividual = async (e) => {
+        e.preventDefault();
+
+        const newIndividual = {
+            id: state.id,
+            nick_name: state.nick_name,
+            species_id: state.species_id,
+            seen_on: state.seen_on,
+        }
+        console.log(newIndividual);
+        const response = await fetch('http://localhost:4040/individuals',{
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newIndividual)
+        });
+        const content = await response.json();
+        setIndividuals([...individuals, content])
+        dispatch({type:'clearForm'});
+    }
+
+    return (
+        <>
+            <header> Individuals Data Table </header>
+            <table>
+                <thead>
+                    <th>ID: </th>
+                    <th>Nickname:  </th>
+                    <th>Seen On: </th>
+                    <th>Species ID: </th>
+                </thead>
+                <tbody>
+                    {individuals.map((individual, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{individual.id}</td>
+                                <td>{individual.nick_name}</td>
+                                <td>{individual.seen_on}</td>
+                                <td>{individual.species_id}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+
+            <div className="addIndividuals">
+                <form id="add-individuals" action="#" onSubmit={handleAddIndividual}>
+                    <fieldset>
+                        <br></br>
+                        <label>ID: </label>
+                        <br></br>
+                        <input
+                            type="number"
+                            id="add-individuals-id"
+                            placeholder="Individuals ID"
+                            value={state.id}
+                            onChange={(e) =>
+                                dispatch({
+                                    type: 'editID',
+                                    payload: e.target.value
+                                })
+                            }
+                        />
+                        <br></br>
+                        <label>Nick Name: </label>
+                        <br></br>
+                        <input
+                            type="text"
+                            id="add-individuals-nick_name"
+                            placeholder="Nick Name"
+                            value={state.nick_name}
+                            onChange={(e) =>
+                                dispatch({
+                                    type: 'editNickName',
+                                    payload: e.target.value
+                                })
+                            }
+                        />
+                        <br></br>
+                        <label>Species Id: </label>
+                        <br></br>
+                        <input
+                            type="number"
+                            id="add-individuals-speciesId"
+                            placeholder="species ID"
+                            value={state.species_id}
+                            onChange={(e) =>
+                                dispatch({
+                                    type: 'editSpeciesId',
+                                    payload: e.target.value
+                                })
+                            }
+                        />
+                        <br></br>
+                        <label>Seen on: </label>
+                        <br></br>
+                        <input
+                            type="datetime-local"
+                            id="add-individuals-seenOn"
+                            placeholder=""
+                            value={state.seen_on}
+                            onChange={(e) =>
+                                dispatch({
+                                    type: 'editSeenOn',
+                                    payload: e.target.value
+                                })
+                            }
+                        />
+                    </fieldset>
+                    {/* Add more form fields here */}
+                    <input type="submit" />
+                </form>
+            </div>
+        </>
     );
 }
 export default IndividualsInfo;
